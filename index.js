fdec189ff706e8bfd969de02b2db23045060a8c6
Netlonde <carloscomercialti@gmail.com>
2022-02-14 15:55:52 -0300

    feat: challenge 31

--- index.js
+++ index.js
@@ -1,40 +1,19 @@
 (function(DOM) {
     'use strict';
   
-    /*
-    Vamos estruturar um pequeno app utilizando módulos.
-    Nosso APP vai ser um cadastro de carros. Vamos fazê-lo por partes.
-    A primeira etapa vai ser o cadastro de veículos, de deverá funcionar da
-    seguinte forma:
-    - No início do arquivo, deverá ter as informações da sua empresa - nome e
-    telefone (já vamos ver como isso vai ser feito)
-    - Ao abrir a tela, ainda não teremos carros cadastrados. Então deverá ter
-    um formulário para cadastro do carro, com os seguintes campos:
-      - Imagem do carro (deverá aceitar uma URL)
-      - Marca / Modelo
-      - Ano
-      - Placa
-      - Cor
-      - e um botão "Cadastrar"
-    Logo abaixo do formulário, deverá ter uma tabela que irá mostrar todos os
-    carros cadastrados. Ao clicar no botão de cadastrar, o novo carro deverá
-    aparecer no final da tabela.
-    Agora você precisa dar um nome para o seu app. Imagine que ele seja uma
-    empresa que vende carros. Esse nosso app será só um catálogo, por enquanto.
-    Dê um nome para a empresa e um telefone fictício, preechendo essas informações
-    no arquivo company.json que já está criado.
-    Essas informações devem ser adicionadas no HTML via Ajax.
-    Parte técnica:
-    Separe o nosso módulo de DOM criado nas últimas aulas em
-    um arquivo DOM.js.
-    E aqui nesse arquivo, faça a lógica para cadastrar os carros, em um módulo
-    que será nomeado de "app".
-    */
+   /*
+  Agora vamos criar a funcionalidade de "remover" um carro. Adicione uma nova
+  coluna na tabela, com um botão de remover.
+  Ao clicar nesse botão, a linha da tabela deve ser removida.
+  Faça um pull request no seu repositório, na branch `challenge-31`, e cole
+  o link do pull request no `console.log` abaixo.
+  Faça um pull request, também com a branch `challenge-31`, mas no repositório
+  do curso, para colar o link do pull request do seu repo.
+  */
 
     function app(){
       return{
         init: function(){
-          console.log("inicou") 
           this.companyInfo();
           this.initEvents();
         },
@@ -43,7 +22,7 @@
           const ajax = new XMLHttpRequest();
           ajax.open('GET', 'company.json', true);
           ajax.send();
-          ajax.addEventListener('readystatechange', this.getCompanyInfo)
+          ajax.addEventListener('readystatechange', this.getCompanyInfo, false)
         },
 
         initEvents: function initEvents(){
@@ -63,32 +42,49 @@
         newCar: function newCar(){
           const $fragment = document.createDocumentFragment();
           const $tr = document.createElement('tr');
+          $tr.id = (Math.random()*100000);
           const $tdImage = document.createElement('td');
           const $image = document.createElement('img');
           const $tdModel = document.createElement('td');
           const $tdYear = document.createElement('td');
           const $tdPlate = document.createElement('td');
           const $tdColor = document.createElement('td');
+          const $removeIten = document.createElement('td');
 
           $image.src = new DOM('.image').get().value;
           $tdModel.textContent = new DOM('.model').get().value;
           $tdYear.textContent = new DOM('.year').get().value;
           $tdPlate.textContent = new DOM('.plate').get().value;
           $tdColor.textContent = new DOM('.color').get().value;
-
           
-          $tdImage.appendChild($image);
-          $tr.appendChild($tdImage);
-          $tr.appendChild($tdModel);
-          $tr.appendChild($tdYear);
-          $tr.appendChild($tdPlate);
-          $tr.appendChild($tdColor);
+          this.createDeleteButton($removeIten, $tr.id);
+          this.createTable($tdImage, $image);
+          this.createTable($tr, $tdImage, $tdModel, $tdYear, $tdPlate, $tdColor, $removeIten)
           
           return $fragment.appendChild($tr);
         },
+
+        createTable(parentElement){
+          for(let elements of arguments){
+            if(elements !== parentElement) parentElement.appendChild(elements);
+          }
+        },
+
+        createDeleteButton($removeIten, id){
+          const $removeButton = document.createElement('button');
+          $removeButton.innerHTML = 'Remover';
+          $removeButton.addEventListener('click',() => this.handleDelete(id));
+          $removeIten.appendChild($removeButton);
+        },
+
+        handleDelete: function(id){
+          const childElement = document.getElementById(id); 
+          const table = document.querySelector('.table')//.removeChild(childElement);
+          table.removeChild(childElement);
+        },
         
         getCompanyInfo: function getCompanyInfo(){
-          if(app().isReady.call(this)) return;
+          if(!app().isReady.call(this)) return;
 
           const data = JSON.parse(this.responseText);
           const $companyName = (new DOM('.company-name')).get();
@@ -98,7 +94,7 @@
         },
 
         isReady: function isReady(){
-          return (this.readState === 4 && this.status === 200)
+          return (this.readyState === 4 && this.status === 200)
         }
       };
     }
